689039cc765651893e7d8c0dc170f6f7bbc072bc
gomsaledt <70487004+gomsaledt@users.noreply.github.com>
2020-08-31 02:09:36 +0530

    Add files via upload

---
+++ Ppl/Assingment2/B-1/B-1.s
@@ -0,0 +1,46 @@
+	.file	"B-1.c"
+	.text
+	.globl	AddTwo
+	.type	AddTwo, @function
+AddTwo:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	addl	$2, -4(%rbp)
+	movl	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	AddTwo, .-AddTwo
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$3, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	call	AddTwo
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
