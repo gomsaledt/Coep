0809b5b650eb773284de36c4ed6c27dc2f6b04f8
gomsaledt <70487004+gomsaledt@users.noreply.github.com>
2020-08-31 02:15:23 +0530

    Add files via upload

---
+++ Ppl/Assingment2/B-2/B-2.s
@@ -0,0 +1,32 @@
+	.file	"B-2.c"
+	.text
+	.comm	N,4,4
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$10, -4(%rbp)
+	movl	$0, -8(%rbp)
+	jmp	.L2
+.L3:
+	movl	N(%rip), %eax
+	addl	%eax, -4(%rbp)
+	addl	$1, -8(%rbp)
+.L2:
+	cmpl	$3, -8(%rbp)
+	jle	.L3
+	movl	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
