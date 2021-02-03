22edf83edddff6f8eaf4b6968dba777973f2f3e8
gomsaledt <gomsaledt19.comp@coep.ac.in>
2021-02-03 20:41:32 +0530

    Print Address

---
+++ Data Structure And Algorithm/C/PrintAddress.c
@@ -0,0 +1,8 @@
+// C program to print address
+#include<stdio.h>
+
+int main(){
+    printf("Name = Dnyaneshwar Tanaji Gomsale\n");
+    printf("Address = Jajnur,\n\tTq. Nilanga,\n\tDist.Latur,\n\tMaharashtra-413521");
+    return 0;
+}
